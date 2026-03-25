09c343dc4a17910e328b5209166cd1df7a2a7a91
sanyich <kozlovasasha77@gmail.com>
2026-03-25 14:44:48 +0000

    initial commit

---
+++ src/main/java/pt/unl/fct/di/adc/firstwebapp/resources/LoginResource.java
@@ -0,0 +1,58 @@
+package pt.unl.fct.di.adc.firstwebapp.resources;
+
+import java.util.logging.Logger;
+
+import jakarta.ws.rs.Consumes;
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.POST;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.PathParam;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.core.Response;
+
+import pt.unl.fct.di.adc.firstwebapp.util.AuthToken;
+import pt.unl.fct.di.adc.firstwebapp.util.LoginData;
+
+import com.google.gson.Gson;
+
+
+@Path("/login")
+@Produces(MediaType.APPLICATION_JSON + ";charset=utf-8")
+public class LoginResource {
+
+	/** 
+	 * Logger Object
+	 */
+	private static final Logger LOG = Logger.getLogger(LoginResource.class.getName());
+
+	private final Gson g = new Gson();
+	
+	public LoginResource() {} // Nothing to be done here
+	
+	@POST
+	@Path("/")
+	@Consumes(MediaType.APPLICATION_JSON)
+	public Response doLogin(LoginData data) {
+		LOG.fine("Attempt to login user: " + data.username);
+
+		if(data.username.equals("jleitao") && data.password.equals("password")) {
+			AuthToken at = new AuthToken(data.username);
+			return Response.ok(g.toJson(at)).build();
+		}
+
+		return Response.status(Response.Status.FORBIDDEN).entity("Incorrect username or password.").build();
+		
+	}
+	
+	@GET
+	@Path("/{username}")
+	public Response checkUsernameAvailable(@PathParam("username") String username) {
+		if(username.trim().equals("jleitao")) {
+			return Response.ok().entity(g.toJson(false)).build();
+		} else {
+			return Response.ok().entity(g.toJson(true)).build();
+		}
+	}
+
+}
